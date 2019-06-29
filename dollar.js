f96761c6fff858bfb66ea9623349bed01e527da2
Belén Abad <belenabadcastro23@gmail.com>
2019-06-29 17:14:10 -0500

    Chapter 10

--- dollar.js
+++ dollar.js
@@ -4,9 +4,6 @@ class Dollar extends Money {
   constructor(amount) {
     super(amount, "USD");
   }
-  times(multiplier) {
-    return new Dollar(this.amount * multiplier);
-  }
 }
 
 module.exports = Dollar;
